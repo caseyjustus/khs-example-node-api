d3936c6268ed756828e854c5a28dabe08dd540bb
Casey Justus <cjustus@keyholesoftware.com>
2017-11-29 15:17:42 -0600

    changed for cli

--- api/controller.js
+++ api/controller.js
@@ -4,7 +4,7 @@ var os = require('os');
 
 function get(req, res) {
    
-    res.send('Hello Casey from - >'+os.hostname());
+    res.send('Hello Casey from the cli at - >'+os.hostname());
            
 }
 
